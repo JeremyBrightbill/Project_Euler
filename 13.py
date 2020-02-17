a24fe5ccde0514c5ec38495e67086e57d6504c8d
Jeremy Brightbill <jeremy.brightbill@gmail.com>
2020-02-17 15:46:45 -0600

    Started Exercise 13

---
+++ 13.py
@@ -0,0 +1,10 @@
+"""
+TO DO: For this one, try requests and BeautifulSoup
+
+
+<div style="font-family:'courier new';font-size:10pt;text-align:center;">"""
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://projecteuler.net/problem=13'
