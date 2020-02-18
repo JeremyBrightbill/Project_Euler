f3f00b9f513990ecf0caed4e2488f378423f4547
Jeremy Brightbill <jeremy.brightbill@gmail.com>
2020-02-18 07:30:26 -0600

    Exercise 13

--- 13.py
+++ 13.py
@@ -1,10 +1,25 @@
-"""
-TO DO: For this one, try requests and BeautifulSoup
+"""WORK IN PROGRESS
 
-
-<div style="font-family:'courier new';font-size:10pt;text-align:center;">"""
+Work out the first ten digits of the sum of the following one-hundred 
+50-digit numbers."""
 
 import requests
 from bs4 import BeautifulSoup
+from typing import List
+
+# Pull data straight from Project Euler page
 
 URL = 'https://projecteuler.net/problem=13'
+
+def get_data(URL: str) -> List[int]:
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.text, features='html.parser')
+    nums = soup.select('[style*="font-size:10pt;text-align:center;"]')[0].text
+    nums = nums.split('\n')
+    nums = [int(value) for value in nums[1:]]
+    return nums
+
+if __name__ == "__main__":
+    
+    data = get_data(URL)
+    print(sum(data))
