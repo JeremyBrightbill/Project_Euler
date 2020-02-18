985d0ef6f46f95fdea7fe8c9509cacfcc5e7a594
Jeremy Brightbill <jeremy.brightbill@gmail.com>
2020-02-18 15:40:23 -0600

    Added exercise title to each

--- 11.py
+++ 11.py
@@ -1,13 +1,14 @@
-"""In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+"""Largest product in a grid
 
-...
+In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
 
 The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
 
 What is the greatest product of four adjacent numbers in the same direction 
 (up, down, left, right, or diagonally) in the 20×20 grid?
 
-SOLUTION: Wanted to pull this with requests and BeautifulSoup, but the HTML is too irregular."""
+SOLUTION: Wanted to pull this with requests and BeautifulSoup, but the HTML 
+is too irregular."""
 
 from typing import List
 
--- 13.py
+++ 13.py
@@ -1,4 +1,6 @@
-"""Work out the first ten digits of the sum of the following one-hundred 
+"""Large sum
+
+Work out the first ten digits of the sum of the following one-hundred 
 50-digit numbers."""
 
 import requests
