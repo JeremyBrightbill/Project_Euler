8e0d2e62fb64a8c478ce35d695e53fd8f803dc9e
Jeremy Brightbill <jeremy.brightbill@gmail.com>
2020-02-18 07:31:01 -0600

    Exercise 13

--- 13.py
+++ 13.py
@@ -1,6 +1,4 @@
-"""WORK IN PROGRESS
-
-Work out the first ten digits of the sum of the following one-hundred 
+"""Work out the first ten digits of the sum of the following one-hundred 
 50-digit numbers."""
 
 import requests
